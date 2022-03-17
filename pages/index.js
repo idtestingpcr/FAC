8e8bccbdb3279afedf071353888cc182331e8f95
alimehdipak <alimehdipak@gmail.com>
2022-03-17 15:14:02 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   
   useEffect(() => {
   // console.log(window.location, '================')
-      axios.get('https://itslyrics.com/wp-json/wp/v2/posts/')
+      axios.get('https://facelive.win/wp-json/wp/v2/posts/')
       .then(res => {
         // console.log(res.data)
         // const personss = res.data;
